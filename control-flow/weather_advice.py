00c4c0c6f1d6d9e06fc4c8af3b7588086a9e1edd
wilson <mutidawilz@gmail.com>
2024-12-17 10:07:04 +0300

    Corrected the cold season

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,11 @@
-weather =input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 if weather == "sunny":
  print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
  print("Don't forget your umbrella and raincoat.")
 elif weather == "cold":
- print("Make sure to wear a warm coat and a scarf")
+ print("Make sure to wear a warm coat and a scarf.")
 else:
- print("Sorry, I dont have recommendations for this weather")
+ print("Sorry, I don't have recommendations for this weather.")
 
