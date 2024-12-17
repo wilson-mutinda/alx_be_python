c15835fe00fa77a2c1d7a465ed5f1cf4314a12c2
wilson <mutidawilz@gmail.com>
2024-12-17 10:31:48 +0300

    Added a file on calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+operation = input("Choose the operation (+, -, *, /): ")
+match operation:
+ case _ if operation == "+":
+  print (f"The result is {num1 + num2}.")
+ case _ if operation == "-":
+  print (f"The result is {num1 - num2}.")
+ case _ if operation == "*":
+  print (f"The result is {num1 * num2}.")
+ case _ if operation == "/":
+  if num2 == 0:
+   print ("Cannot devide by 0")
+  else:
+   print (f"The result is {num1 / num2}.")
+ case _:
+  print ("Invalid operation")
